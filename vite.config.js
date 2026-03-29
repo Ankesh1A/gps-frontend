e158558770e127b7735db7a5319c242736f05f5d
tera-username <tera-email>
2026-03-29 19:48:46 +0530

    vite conig

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://gps-backend-p7nv.onrender.com',
         changeOrigin: true,
       }
     }
